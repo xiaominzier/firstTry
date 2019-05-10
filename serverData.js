8f7b6e544c12178550ce2e1b7298bf5b5b1f8113
xiaominzier <49120089+xiaominzier@users.noreply.github.com>
2019-05-10 16:04:13 +0800

    jquery小demo之热点排名

---
+++ serverData.js
@@ -0,0 +1,16 @@
+var data=[
+    {title:'王俊凯同班同学',search:468,hisSearch:799},
+    {title:'王俊凯同班同学',search:468,hisSearch:799},
+    {title:'王俊凯同班同学',search:468,hisSearch:799},
+    {title:'王俊凯同班同学',search:468,hisSearch:799},
+    {title:'王俊凯同班同学',search:468,hisSearch:799},
+    {title:'王俊凯同班同学',search:468,hisSearch:799},
+    {title:'王俊凯同班同学',search:468,hisSearch:799},
+    {title:'王俊凯同班同学',search:468,hisSearch:799},
+    {title:'王俊凯同班同学',search:468,hisSearch:799},
+    {title:'王俊凯同班同学',search:468,hisSearch:799},
+    {title:'王俊凯同班同学',search:468,hisSearch:799},
+    {title:'王俊凯同班同学',search:468,hisSearch:799},
+    {title:'王俊凯同班同学',search:468,hisSearch:799},
+    {title:'王俊凯同班同学',search:468,hisSearch:799}
+]
